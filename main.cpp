4da5dd7d730e78d866d4d96e2d9ce204153b9296
Thyago Barbosa Rodrigues <thyagobr@gmail.com>
2015-04-21 14:25:23 -0300

    Createss a backbuffer for rendering information.
    Apparently you can't just create a pointer to a struct. I tried
    and got errors when malloc() was ran. Now that backbuffer isn't a
    pointer anymore, it works.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,46 @@
 #include <SDL.h>
 
+struct Backbuffer {
+  void *memory;
+  int width;
+  int height;
+} backbuffer;
+
+void render(void *backbuffer)
+{
+
+}
+
+
 int main(int argc, char *argv[])
 {
   SDL_Event event;
   SDL_Window *window;
   SDL_Renderer *renderer;
+  SDL_Texture *texture;
   bool game_running = true;
+  backbuffer.width = 1280;
+  backbuffer.height = 720;
+  backbuffer.memory = malloc(1280 * 720 * 4);
+
 
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
   {
     printf("SDL couldn't be initialized\n %s\n", SDL_GetError());
     return(100);
   }
+
   SDL_CreateWindowAndRenderer(1280, 720, SDL_WINDOW_RESIZABLE, &window, &renderer);
   if ((!window) || (!renderer))
   {
     printf("rakshaza: window creation failed.\n %s\n", SDL_GetError());
     return(101);
   }
+
+  texture = SDL_CreateTexture(renderer,
+      SDL_PIXELFORMAT_ARGB8888,
+      SDL_TEXTUREACCESS_STREAMING,
+      1280, 720);
 
   while (game_running)
   {
@@ -33,6 +56,7 @@ int main(int argc, char *argv[])
     }
   }
 
+  free(backbuffer.memory);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
 }

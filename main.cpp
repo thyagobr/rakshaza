2cc3df16bf57438b9a4469c6172fcb8f18b793f2
Thyago Barbosa Rodrigues <thyagobr@gmail.com>
2015-04-21 17:54:29 -0300

    Adds resizing capabilities to rendering.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <SDL.h>
+#include <time.h>
+
+Uint64 performance_frequency;
+Uint64 last_counter;
+Uint64 end_counter;
 
 struct Backbuffer {
   void *memory;
@@ -15,6 +20,7 @@ void render()
     for (int w = 0; w < backbuffer.width; ++w)
     {
       *pixel++ = 0x912CEE;
+      // *pixel++ = rand() % 4294967296;
     }
   } 
 }
@@ -22,6 +28,7 @@ void render()
 
 int main(int argc, char *argv[])
 {
+  performance_frequency = SDL_GetPerformanceFrequency();
   SDL_Event event;
   SDL_Window *window;
   SDL_Renderer *renderer;
@@ -29,7 +36,7 @@ int main(int argc, char *argv[])
   bool game_running = true;
   backbuffer.width = 1280;
   backbuffer.height = 720;
-  backbuffer.memory = malloc(1280 * 720 * 4);
+  backbuffer.memory = malloc(backbuffer.width * backbuffer.height * 4);
 
 
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
@@ -38,7 +45,7 @@ int main(int argc, char *argv[])
     return(100);
   }
 
-  SDL_CreateWindowAndRenderer(1280, 720, SDL_WINDOW_RESIZABLE, &window, &renderer);
+  SDL_CreateWindowAndRenderer(backbuffer.width, backbuffer.height, SDL_WINDOW_RESIZABLE, &window, &renderer);
   if ((!window) || (!renderer))
   {
     printf("rakshaza: window creation failed.\n %s\n", SDL_GetError());
@@ -48,12 +55,13 @@ int main(int argc, char *argv[])
   texture = SDL_CreateTexture(renderer,
       SDL_PIXELFORMAT_ARGB8888,
       SDL_TEXTUREACCESS_STREAMING,
-      1280, 720);
+      backbuffer.width, backbuffer.height);
 
   while (game_running)
   {
+    last_counter = SDL_GetPerformanceCounter();
     render();
-    SDL_UpdateTexture(texture, NULL, backbuffer.memory, 1280 * sizeof(Uint32));
+    SDL_UpdateTexture(texture, NULL, backbuffer.memory, backbuffer.width * sizeof(Uint32));
 
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, NULL, NULL);
@@ -67,8 +75,24 @@ int main(int argc, char *argv[])
           printf("Quitting.\n");
           game_running = false;
           break;
+        case SDL_WINDOWEVENT:
+          {
+          switch(event.window.event)
+          {
+            case SDL_WINDOWEVENT_RESIZED:
+              {
+                backbuffer.width = event.window.data1;
+                backbuffer.height = event.window.data2;
+                free(backbuffer.memory);
+                backbuffer.memory = malloc(backbuffer.width * backbuffer.height * 4);
+              } break;
+          }
+          } break;
       }
     }
+    end_counter = SDL_GetPerformanceCounter();
+    printf("ms per frame: %f\n", (((float) (end_counter - last_counter) * 1000.0f) / (float) performance_frequency));
+    last_counter = end_counter;
   }
 
   free(backbuffer.memory);

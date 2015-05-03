5aa9e9afddaaa437a643aad5b5afc0801a51ae67
Thyago Barbosa Rodrigues <thyagobr@gmail.com>
2015-05-03 11:50:27 -0300

    Changes compiler to gcc.

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@
 exec > /dev/null
 mkdir -p build
 pushd build
-c++ ../main.cpp -o rakshaza -g -DRAKSHAZA_INTERNAL=1 -DRAKSHAZA_DEBUG=1 `sdl2-config --cflags --libs`
+gcc ../main.cpp -o rakshaza -g -DRAKSHAZA_INTERNAL=1 -DRAKSHAZA_DEBUG=1 `sdl2-config --cflags --libs` -lm
 popd
